821b8c6458299d20bf0fe28243628dd28e9a10ab
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-05 00:04:38 -0500

    add axios, update css, add alt tag

--- src/app/navbar/NavBar.js
+++ src/app/navbar/NavBar.js
@@ -9,7 +9,7 @@ function NavBar(props) {
     return (
     <header className="navbar">
         <div>
-            <img className="nav-logo" src="https://raw.githubusercontent.com/Fun2LearnCode/company-logo-assets/master/resources/exported%20pictures/f2lc-logo.png?token=AJvhOqDp9aWM4gIruJw3aa4_8PxjHUQSks5bmGk8wA%3D%3D"></img>
+            <img alt="Fun 2 Learn Code logo"className="nav-logo" src="https://raw.githubusercontent.com/Fun2LearnCode/company-logo-assets/master/resources/exported%20pictures/f2lc-logo.png?token=AJvhOqDp9aWM4gIruJw3aa4_8PxjHUQSks5bmGk8wA%3D%3D"></img>
         </div>
         <div className="nav-item-container">
             {routesNameList}
