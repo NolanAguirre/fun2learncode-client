9d0584f3a4cc58176be3a573082b0abd53be1fef
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-06 21:01:56 -0500

    update activity styling

--- src/app/main/activities/activity/Activity.js
+++ src/app/main/activities/activity/Activity.js
@@ -2,7 +2,25 @@ import React from 'react';
 import './Activity.css';
 import { Link } from "react-router-dom";
 function Activity(props) {
-    return<div>{props.name}{props.description}<Link to={`/Events/${props.id}`}><button>{props.id}</button></Link></div>
+    return(<div className="activity-container">
+            <div className="activity-header">
+                <img src="https://via.placeholder.com/350x150"></img>
+                <div className="activity-header-text">
+                    <h2 className="activity-title">
+                        {props.name}
+                    </h2>
+                    <div>
+                        prerequisites
+                    </div>
+                </div>
+                <div className="activity-view-events">
+                    <Link to={`/Events/${props.id}`}><button className="activity-view-events-btn">View Event Dates</button></Link>
+                </div>
+            </div>
+            <div className="activity-body">
+                <div>{props.description}</div>
+            </div>
+        </div>)
 }
 
 export default Activity;
