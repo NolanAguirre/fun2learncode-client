745c11481ab3839046297abb3412da1fe1d69b80
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-11 21:46:22 -0500

    add redirect if not login

--- src/app/main/registration/Registration.js
+++ src/app/main/registration/Registration.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
 import './Registration.css';
+import Login from '../login/Login';
 import axios from 'axios';
 class Registration extends Component{
     render(){
-        return (<div className="registration-container"></div>);
+        if(this.props.user){
+            return (<div className="registration-container"></div>);
+        }else{
+            console.log(this.props);
+            return (<Login history={this.props.history} redirectUrl={this.props.location.pathname}></Login>)
+        }
     }
 }
 
