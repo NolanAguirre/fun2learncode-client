f0e710ccc1ae6654d6667c2192a1581d5bbc99f6
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-11 20:50:01 -0500

    move navbar routes to navbar, add test function

--- src/app/navbar/NavBar.js
+++ src/app/navbar/NavBar.js
@@ -1,16 +1,79 @@
 import React from 'react';
-import './NavBar.css'
-import Item from './item/Item'
+import './NavBar.css';
+import Item from './item/Item';
+import Logo from '../drawing.svg';
+const routeNames = [
+    {
+        name: 'Home',
+        route: 'Home',
+        test: (prop) => {
+            return !prop.user
+        }
+    }, {
+        name: 'About Us',
+        route: 'About Us',
+        test: (prop) => {
+            return !prop.user
+        }
+    }, {
+        name: 'Summer Camps',
+        route: 'Activity/Summer Camps',
+        test: (prop) => {
+            return !prop.user || prop.user.role == "FTLC_USER"
+        }
+    }, {
+        name: 'Classes',
+        route: 'Activity/Classes',
+        test: (prop) => {
+            return !prop.user || prop.user.role == "FTLC_USER"
+        }
+    }, {
+        name: 'Labs',
+        route: 'Activity/Labs',
+        test: (prop) => {
+            return !prop.user || prop.user.role == "FTLC_USER"
+        }
+    }, {
+        name: 'Workshops',
+        route: 'Activity/Workshops',
+        test: (prop) => {
+            return !prop.user || prop.user.role == "FTLC_USER"
+        }
+    }, {
+        name: 'Login',
+        route: 'Login',
+        test: (prop) => {
+            return !prop.user
+        }
+    }, {
+        name: 'Manage Students',
+        route: 'User/Manage Students',
+        test: (prop) => {
+            return prop.user && prop.user.role == "FTLC_USER"
+        }
+    },{
+        name: 'Account',
+        route: 'User/Account',
+        test: (prop) => {
+            return prop.user && prop.user.role == "FTLC_USER"
+        }
+    }, {
+        name: 'Logout',
+        route: 'Logout',
+        test: (prop) => {
+            return prop.user
+        }
+    }
+
+];
 function NavBar(props) {
-    const routeNames = props.routeNames;
-    const routesNameList = routeNames.map((routeObj) => {
-        return <Item key={routeObj.name} route={routeObj.route} name={routeObj.name}></Item>;
+    const routesNameList = routeNames.filter((routeObj) => {
+        return routeObj.test(props)
+    }).map((routeObj) => {
+        return <Item key={routeObj.route} route={routeObj.route} name={routeObj.name}></Item>;
     })
-    return (
-    <header className="navbar">
-        <div>
-            <img alt="Fun 2 Learn Code logo"className="nav-logo" src="https://raw.githubusercontent.com/Fun2LearnCode/company-logo-assets/master/resources/exported%20pictures/f2lc-logo.png?token=AJvhOqDp9aWM4gIruJw3aa4_8PxjHUQSks5bmGk8wA%3D%3D"></img>
-        </div>
+    return (<header className="navbar">
+        <img alt="Fun 2 Learn Code logo" src={Logo}></img>
         <div className="nav-item-container">
             {routesNameList}
         </div>
