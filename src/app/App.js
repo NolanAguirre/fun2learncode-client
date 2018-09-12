eff4c224b048460429fa5893e1129de24cc22f5e
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-11 20:39:16 -0500

    update app component to apollo

--- src/app/App.js
+++ src/app/App.js
@@ -3,13 +3,53 @@ import './App.css';
 import NavBar from './navbar/NavBar';
 import Main from './main/Main';
 import Footer from './footer/Footer'
-
+import axios from 'axios';
+import UserStore from './Store'
+import gql from 'graphql-tag';
+import {Query} from 'react-apollo';
+const GET_USER = gql`
+    {
+      getUserData{
+        firstName
+        lastName
+        role
+        id
+      }
+  }`
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            authToken: localStorage.getItem('authToken')
+        };
+        UserStore.addEvent({authToken:(token)=>{this.handleAuthToken(token)}})
+    }
+    handleAuthToken(obj){
+        this.setState(obj)
+        if(obj.authToken){
+            localStorage.setItem('authToken', obj.authToken);
+        }else{
+            localStorage.removeItem('authToken');
+        }
+    }
     render() {
-        return (
-        <div className="app-container">
-            <NavBar routeNames={this.props.routeNames}></NavBar>
-            <Main></Main>
+        return (<div className="app-container">
+        <Query query={GET_USER}>{
+            ({loading, error, data}) => {
+                if (loading) {
+                    return 'Loading...';
+                }
+                if (error) {
+                    return `Error! ${error.message}`;
+                }
+                return(
+                    <div className="app-inner">
+                        <NavBar user={data.getUserData}></NavBar>
+                        <Main user={data.getUserData}></Main>
+                    </div>
+                );
+        }
+            }</Query>
             <Footer></Footer>
         </div>);
     }
