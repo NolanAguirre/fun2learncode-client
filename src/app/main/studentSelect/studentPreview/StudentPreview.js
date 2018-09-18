369a58200a856260f054a225b3f0490489b2d908
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-17 19:42:40 -0500

    simplify student preview

--- src/app/main/studentSelect/studentPreview/StudentPreview.js
+++ src/app/main/studentSelect/studentPreview/StudentPreview.js
@@ -4,22 +4,10 @@ class StudentPreview extends Component{
     render(){
         return (
             <div onClick={()=>{this.props.click(this.props.id)}}className="student-preview-container">
-                <div className="student-preview-header">
-                    <img src="https://via.placeholder.com/100x100"></img>
-                    <div className="student-preview-info">
+                <div className="student-preview-info">
                     {this.props.student.firstName} {this.props.student.lastName}
-                    </div>
-                </div>
-                <div>
-                    I Dont Know what information will go here<br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
                 </div>
             </div>);
-
     }
 }
-
 export default StudentPreview;
