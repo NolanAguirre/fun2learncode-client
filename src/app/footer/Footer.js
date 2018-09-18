e475d0cd7e89fac9332c1a3d43af5c9671747144
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-18 14:05:58 -0500

    implement basic footer

--- src/app/footer/Footer.js
+++ src/app/footer/Footer.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 import './Footer.css';
+import FacebookLogo from '../logos/ionicons-logo-facebook.svg';
+import TwitterLogo from '../logos/ionicons-logo-twitter.svg';
+import EmailIcon from '../logos/ionicons-md-mail.svg';
 class Footer extends Component {
     render() {
         return (
-        <div className="footer">
-            This is the footer
-        </div>);
+            <div className="footer">
+                <div className="footer-content">
+                    <a href="https://www.facebook.com/Fun2LearnCode/"><img alt="Fun2LearnCode Facebook" src={FacebookLogo}></img></a>
+                    <a href="https://twitter.com/fun2learncode?lang=en"><img alt="Fun2LearnCode Twitter" src={TwitterLogo}></img></a>
+                    <img alt="Fun2LearnCode Email" src={EmailIcon}></img>
+                </div>
+            </div>
+        );
     }
 }
 
