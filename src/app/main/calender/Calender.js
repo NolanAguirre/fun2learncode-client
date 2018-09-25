4e131eee5ea661a4092e9e55be8ff40223ddf806
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-25 14:58:05 -0500

    add working version of calander drag and drop

--- src/app/main/calender/Calender.js
+++ src/app/main/calender/Calender.js
@@ -50,17 +50,55 @@ function Calender(props){
 
 const DraggableCalender = withDragAndDrop(BigCalendar)
 
+const events = [
+  {
+    id: 0,
+    title: 'Board meeting',
+    start: new Date(2018, 0, 29, 9, 0, 0),
+    end: new Date(2018, 0, 29, 13, 0, 0),
+    resourceId: 1,
+  },
+  {
+    id: 1,
+    title: 'MS training',
+    start: new Date(2018, 0, 29, 14, 0, 0),
+    end: new Date(2018, 0, 29, 16, 30, 0),
+    resourceId: 2,
+  },
+  {
+    id: 2,
+    title: 'Team lead meeting',
+    start: new Date(2018, 0, 29, 8, 30, 0),
+    end: new Date(2018, 0, 29, 12, 30, 0),
+    resourceId: 3,
+  },
+  {
+    id: 11,
+    title: 'Birthday Party',
+    start: new Date(2018, 0, 30, 7, 0, 0),
+    end: new Date(2018, 0, 30, 10, 30, 0),
+    resourceId: 4,
+  },
+]
+
+const resourceMap = [
+  { resourceId: 1, resourceTitle: 'Board room' },
+  { resourceId: 2, resourceTitle: 'Training room' },
+  { resourceId: 3, resourceTitle: 'Meeting room 1' },
+  { resourceId: 4, resourceTitle: 'Meeting room 2' },
+]
+
 class DragAndDropCalendar extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      events: [],
+      events: events,
     }
-
     this.moveEvent = this.moveEvent.bind(this)
+    this.newEvent = this.newEvent.bind(this)
   }
 
-  moveEvent({ event, start, end, isAllDay: droppedOnAllDaySlot }) {
+  moveEvent({ event, start, end, resourceId, isAllDay: droppedOnAllDaySlot }) {
     const { events } = this.state
 
     const idx = events.indexOf(event)
@@ -72,7 +110,7 @@ class DragAndDropCalendar extends Component {
       allDay = false
     }
 
-    const updatedEvent = { ...event, start, end, allDay }
+    const updatedEvent = { ...event, start, end, resourceId, allDay }
 
     const nextEvents = [...events]
     nextEvents.splice(idx, 1, updatedEvent)
@@ -82,11 +120,9 @@ class DragAndDropCalendar extends Component {
     })
   }
 
-  resizeEvent = (interactionInfo) => {
+  resizeEvent = ({ event, start, end }) => {
     const { events } = this.state
-    const event = interactionInfo.event;
-    const start = interactionInfo.start;
-    const end = interactionInfo.end;
+
     const nextEvents = events.map(existingEvent => {
       return existingEvent.id == event.id
         ? { ...existingEvent, start, end }
@@ -97,23 +133,39 @@ class DragAndDropCalendar extends Component {
       events: nextEvents,
     })
   }
-
+  newEvent(event) {
+      let idList = this.state.events.map(a => a.id)
+      let newId = Math.max(...idList) + 1
+      let hour = {
+        id: newId,
+        title: 'New Event',
+        allDay: event.slots.length == 1,
+        start: event.start,
+        end: event.end,
+      }
+      this.setState({
+        events: this.state.events.concat([hour]),
+      })
+    }
   render() {
     return (
       <DraggableCalender
-        className={this.props.className}
         selectable
         localizer={localizer}
         events={this.state.events}
         onEventDrop={this.moveEvent}
+        onSelectSlot={this.newEvent}
         resizable
+        resources={resourceMap}
+        resourceIdAccessor="resourceId"
+        resourceTitleAccessor="resourceTitle"
         onEventResize={this.resizeEvent}
-        views={["month"]}
+        views={['month']}
+        defaultDate={new Date(2018, 0, 29)}
       />
     )
   }
 }
-
 export {DragAndDropCalendar};
 
 export {Calender};
