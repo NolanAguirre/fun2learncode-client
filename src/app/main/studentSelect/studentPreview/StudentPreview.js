7536689279f3dc093b43949e16a2886937657401
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-27 14:38:13 -0500

    change student preview to be stateless

--- src/app/main/studentSelect/studentPreview/StudentPreview.js
+++ src/app/main/studentSelect/studentPreview/StudentPreview.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import './StudentPreview.css';
-class StudentPreview extends Component{
-    render(){
-        return (
-            <div onClick={()=>{this.props.click(this.props.id)}} className="student-preview-container">
-                <div className="student-preview-info">
-                    {this.props.student.firstName} {this.props.student.lastName}
-                </div>
-            </div>);
-    }
+
+function StudentPreview(props){
+    return (
+        <div onClick={()=>{props.click(props.id)}} className="student-preview-container">
+            <div className="student-preview-info">
+                {props.student.firstName} {props.student.lastName}
+            </div>
+        </div>);
 }
 export default StudentPreview;
