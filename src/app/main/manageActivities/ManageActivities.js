727c1f43627a5000b7709fe8ea4788135e367c53
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-10-02 13:44:45 -0500

    update manage actvities to use query handler

--- src/app/main/manageActivities/ManageActivities.js
+++ src/app/main/manageActivities/ManageActivities.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import gql from 'graphql-tag';
+import memoize from "memoize-one";
 import {Query, Mutation} from 'react-apollo';
 import {DropDown} from '../common/Common';
 import './ManageActivities.css';
 import Activity from '../activities/activity/Activity';
-const GET_ADDRESSES = gql`{
+import QueryHandler from '../queryHandler/QueryHandler';
+const GET_ADDRESSES = gql `{
 allActivityCatagories{
    edges{
      node{
@@ -36,21 +38,46 @@ allActivities{
     }
   }
 }`
-const MAKE_ACTIVITY = gql`
+const MAKE_ACTIVITY = gql `
 mutation($type:UUID!, $name:String!, $description:String!, $prerequisite:UUID){
   makeActivity(input:{arg0:$type, arg1:$name,arg2:$description,arg3:$prerequisite}){
     clientMutationId
   }
 }
-    `
-
-class ManageActivities extends Component{
+`;
+function ManageActivities(props) {
+    return <QueryHandler query={GET_ADDRESSES}
+        child={(data) => {return <ManageActivitiesClass queryResult={data} {...props}/>}} />
+}
+class ManageActivitiesClass extends Component {
     constructor(props) {
         super(props);
-        this.state = {type:"none", prerequisite:"none", name:"", description:""};
+        this.state = {
+            type: "none",
+            prerequisite: "none",
+            name: "",
+            description: ""
+        };
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+
     }
+    mapTypes = memoize(
+        (data) =>  data.edges.map((element) =>  {return{name: element.node.name, value: element.node.id}})
+    );
+    mapPrerequisites = memoize(
+        (data) => data.edges.map((element) => {return {name: element.node.name + " (" + element.node.activityCatagoryByType.name + ")",value: element.node.id}})
+    );
+    mapActivities = memoize(
+        (data) => data.edges.map((element) => {let temp = element.node.eventPrerequisitesByPrerequisite.edges.map((el) => {return el.node.activityByEvent});
+            return {
+                name: element.node.name + " (" + element.node.activityCatagoryByType.name + ")",
+                description: element.node.description,
+                value: element.node.id,
+                prerequisite: temp
+            }
+        })
+    );
     handleInputChange(event) {
         const target = event.target;
         const value = target.type === 'checkbox'
@@ -59,109 +86,77 @@ class ManageActivities extends Component{
         const name = target.name;
         this.setState({[name]: value});
     }
-    handleSubmit(event, mutation){
+    handleSubmit(event, mutation) {
         event.preventDefault();
-        if(this.hasRequiredValues(this.state)){
-            mutation({variables:this.formatVariables(this.state)});
-            this.setState({
-                type:"none",
-                prerequisite: "none",
-                name: "",
-                description: ""
-            })
-        }else{
+        if (this.hasRequiredValues(this.state)) {
+            mutation({
+                variables: this.formatVariables(this.state)
+            });
+            this.setState({type: "none", prerequisite: "none", name: "", description: ""})
+        } else {
             alert("Please fill in name, catagory and description at least");
         }
     }
-    hasRequiredValues(state){
+    hasRequiredValues(state) {
         return state.type != 'none' && state.name != "" && state.description != "";
     }
-    formatVariables(state){
+    formatVariables(state) {
         let temp = Object.assign({}, state);
-        if(temp.prerequisite === 'none'){
+        if (temp.prerequisite === 'none') {
             delete temp.prerequisite;
         }
         return temp;
     }
-    render(){
-        return(<Query query={GET_ADDRESSES}>
-            {
-                ({loading, error, data}) => {
-                    if (loading) {
-                        return 'Loading...';
-                    }
-                    if (error) {
-                        return `Error! ${error.message}`;
-                    }
-                    let type = data.allActivityCatagories.edges.map((element)=>{return{name:element.node.name,value:element.node.id}});
-                    let prerequisites = data.allActivities.edges.map((element)=>{
-                        return{
-                            name: element.node.name + " (" + element.node.activityCatagoryByType.name + ")",
-                            value: element.node.id
+    render() {
+        const types = this.mapTypes(this.props.queryResult.allActivityCatagories);
+        const prerequisites = this.mapPrerequisites(this.props.queryResult.allActivities);
+        const activities = this.mapActivities(this.props.queryResult.allActivities);
+        return (<div className="manage-activities-container">
+            <div className="manage-activities-form-container">
+                <div>
+                    <h2>Create</h2>
+                    <Mutation ignoreResults="ignoreResults" mutation={MAKE_ACTIVITY}>
+                        {
+                            (makeActivity, {data}) => (<form onSubmit={(e) => {
+                                    this.handleSubmit(e, makeActivity)
+                                }}>
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <td>Name:</td>
+                                            <td>
+                                                <input value={this.state.name} name="name" onChange={this.handleInputChange}></input>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Catagory:</td>
+                                            <td><DropDown options={types} name="type" value={this.state.type} onChange={this.handleInputChange}/>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Description:</td>
+                                            <td>
+                                                <textarea className="manage-activity-textarea"name="description" value={this.state.description} onChange={this.handleInputChange}></textarea>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Prerequisite:</td>
+                                            <td><DropDown options={prerequisites} name="prerequisite" value={this.state.prerequisite} onChange={this.handleInputChange}/>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <button type="submit">Create Activity</button>
+                            </form>)
                         }
-                    })
-                    let activities = data.allActivities.edges.map((element)=>{
-                        let temp = element.node.eventPrerequisitesByPrerequisite.edges.map((el)=>{return el.node.activityByEvent});
-                        return {
-                            name:element.node.name + " (" + element.node.activityCatagoryByType.name + ")",
-                            description:element.node.description,
-                            value:element.node.id,
-                            prerequisite:temp
-                        }
-                    })
-                    return (
-                    <div className="manage-activities-container">
-                        <div className="manage-activities-form-container">
-                            <div>
-                            <h2>Create</h2>
-                                <Mutation ignoreResults mutation={MAKE_ACTIVITY}>
-                                 {(makeActivity, { data }) => (
-                                    <form onSubmit={(e)=>{this.handleSubmit(e,makeActivity)}}>
-                                        <table>
-                                            <tbody>
-                                                <tr>
-                                                    <td>Name:</td>
-                                                    <td><input value={this.state.name} name="name" onChange={this.handleInputChange}></input></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Catagory:</td>
-                                                    <td><DropDown
-                                                        options={type}
-                                                        name="type"
-                                                        value={this.state.type}
-                                                        onChange={this.handleInputChange} />
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Description:</td>
-                                                    <td><textarea value={this.state.description}className="manage-activity-textarea"name="description" onChange={this.handleInputChange}></textarea></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Prerequisite:</td>
-                                                    <td><DropDown
-                                                        options={prerequisites}
-                                                        name="prerequisite"
-                                                        value={this.state.prerequisite}
-                                                        onChange={this.handleInputChange} />
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                        <button type="submit">Create Activity</button>
-                                    </form>)}
-                                </Mutation>
-                            </div>
-                        </div>
-                        <div className="manage-activities-preview">
-                        <h2>Preview</h2>
-                        <Activity
-                            name={this.state.name}
-                            description={this.state.description}/>
-                        </div>
-                    </div>);
-                }
-            }
-        </Query>);
+                    </Mutation>
+                </div>
+            </div>
+            <div className="manage-activities-preview">
+                <h2>Preview</h2>
+                <Activity name={this.state.name} description={this.state.description}/>
+            </div>
+        </div>);
     }
 }
 export default ManageActivities;
