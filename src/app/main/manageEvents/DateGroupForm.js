ef3a764928f913e8751a72a0cfea2c360bee2a60
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-10-06 11:42:50 -0500

    add date group form component

---
+++ src/app/main/manageEvents/DateGroupForm.js
@@ -0,0 +1,122 @@
+import React, {Component} from 'react';
+import {DropDown} from '../common/Common';
+import './DateGroupForm.css';
+import gql from 'graphql-tag';
+import {Query, Mutation} from 'react-apollo';
+import DateTime from 'react-datetime';
+import '../../../react-datetime.css'
+import memoize from "memoize-one";
+import Colors from '../calendar/Colors'
+import EventsPreview from './EventsPreview';
+import moment from 'moment';
+
+const CREATE_DATE_GROUP = gql`
+mutation($dateGroup:DateGroupInput!){
+	createDateGroup(input:{dateGroup:$dateGroup}){
+    dateGroup{
+      id
+      openRegistration
+      closeRegistration
+      event
+    }
+  }
+}`;
+function CreateDates(props) {
+    return (
+        <div className="date-form">
+        <Mutation onCompleted={props.createDateGroup} mutation={CREATE_DATE_GROUP}>
+            {
+                (createDateGroup, {loading, error, data}) => {
+                    if (loading) {
+                        return 'Loading...';
+                    }
+                    if (error) {
+                        return `Error! ${error.message}`;
+                    }
+                    return(<form onSubmit={(e) => {props.handleSubmit(e, createDateGroup)}}>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Open Group On:</td>
+                                <td><DateTime className="full-date-input" dateFormat="MMMM Do YYYY" timeFormat={false} value={props.open} onChange={(time) => {props.handleTimeChange(time, "groupOpen")}}/></td>
+                            </tr>
+                            <tr>
+                                <td>Close Group On:</td>
+                                <td><DateTime className="full-date-input" dateFormat="MMMM Do YYYY" timeFormat={false} value={props.close}  onChange={(time) => {props.handleTimeChange(time, "groupClose")}}/></td>
+                            </tr>
+                            <tr>
+                                <td><button type="submit">Create</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    </form>)
+                }
+            }
+        </Mutation>
+</div>
+);
+}
+
+class DateGroupForm extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: new Date(),
+            close: new Date(+ new Date() + 86400000),
+            displayForm: false
+        }
+    }
+    handleTimeChange = (movement, name)=> {
+        this.setState({[name]: movement})
+    }
+    handleSubmit = (event, mutation) => {
+        event.preventDefault();
+        let dateGroup={
+            event: this.props.eventId,
+            openRegistration: this.state.open,
+            closeRegistration: this.state.close
+        }
+        mutation({
+            variables: {"dateGroup":dateGroup}
+        });
+    }
+    createDateGroup = (data) => {
+        let dateGroup = {
+            ...data.createDateGroup.dateGroup,
+            dates:[]
+        }
+        this.props.createDateGroup(dateGroup);
+        this.setState({displayForm:false});
+    }
+    displayForm = () =>{
+        this.setState({displayForm:true});
+    }
+    render() {
+        return (
+        <div className="manage-events-event-preview">
+            {
+                (this.state.displayForm)?
+                    <CreateDates
+                    open={this.state.open}
+                    close={this.state.close}
+                    handleTimeChange={this.handleTimeChange}
+                    createDateGroup={this.createDateGroup}
+                    handleSubmit={this.handleSubmit}/>:
+                    <div className="event-preview-event-button" onClick={this.displayForm}>New Date Group</div>
+            }
+        </div>)
+    }
+}
+
+// <tr>
+//     <td>Event Start Time:</td>
+//     <td><DateTime className="time-input" dateFormat={false} value={props.start} onChange={(time) => {props.handleTimeChange(time, "start")}}/></td>
+// </tr>
+// <tr>
+//     <td>Event End Time:</td>
+//     <td><DateTime className="time-input" value={props.end} dateFormat={false} onChange={(time) => {props.handleTimeChange(time, "end")}}/></td>
+// </tr>
+// start: new Date().setHours(12,0,0),
+// end: new Date().setHours(13,0,0),
+export default DateGroupForm;
