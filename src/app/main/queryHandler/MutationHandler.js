17e09ab308ed1071238675db89889c95e88dc56c
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-10-15 13:47:26 -0500

    add a mutation handler

---
+++ src/app/main/queryHandler/MutationHandler.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import {Mutation} from 'react-apollo';
+
+function MutationHandler(props){
+    return(
+    <Mutation onCompleted={props.onMutationCompleted} mutation={props.mutation}>
+        {
+            (mutation, {loading, error, data}) => {
+                if (loading) {
+                    return 'Loading...';
+                }
+                if (error) {
+                    if(error.message.includes("permission denied")){
+                        return "You're not authorized to make this request, or you need to log out and back in.";
+                    }
+                    return `Error! ${error.message}`;
+                }
+                return(
+                    <form onSubmit={(e) => {props.handleMutation(e, mutation)}}>
+                        {props.form}
+                    </form>
+                );
+            }
+        }
+    </Mutation>
+);
+}
+
+export default MutationHandler;
