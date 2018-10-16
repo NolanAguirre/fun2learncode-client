4197a3906e89e47393a00a495bf415ba1a05cdab
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-10-15 23:57:46 -0500

    various changes, use apollo as state management

--- src/app/main/manageEvents/EventsPreview.js
+++ src/app/main/manageEvents/EventsPreview.js
@@ -2,49 +2,52 @@ import React, {Component} from 'react';
 import {DropDown} from '../common/Common';
 import './EventsPreview.css';
 import gql from 'graphql-tag';
-import {Query} from 'react-apollo';
+import QueryHandler from '../queryHandler/QueryHandler';
 import DateTime from 'react-datetime';
 import '../../../react-datetime.css'
 import memoize from "memoize-one";
-import Colors from '../calendar/Colors'
+import Colors from '../calendar/Colors';
 import moment from 'moment';
+
 function DateGroup(props){
-    if(!props.dateGroup.dates){
+    //console.log(props);
+    if(false){
         return<div>not working</div>
     }
-    const dates = props.dateGroup.dates.map((element)=>{return <div>{element}</div>})
-        console.log(props);
+    const dates = props.dateGroup.datesJoinsByDateGroup.edges.map((element)=>{
+        return <div key={element.node.id}>{element.node.dateIntervalByDateInterval.start}</div>})
     return(
-        <div onClick={props.setActiveDateGroup} className="event-preview-date-container">
-            <h4>open: {moment(props.dateGroup.open).format("MMMM do YYYY")}</h4>
-            <h4>close: {moment(props.dateGroup.close).format("MMMM do YYYY")}</h4>
+        <div onClick={()=>{props.setActiveDateGroup(props.dateGroup, props.event)}} className="event-preview-date-container">
+            <h4>open: {moment(props.dateGroup.openRegistration).format("MMMM do YYYY")}</h4>
+            <h4>close: {moment(props.dateGroup.closeRegistration).format("MMMM do YYYY")}</h4>
+            <h4>Start: {moment(props.dateGroup.start).format("h:mm a")}</h4>
+            <h4>End: {moment(props.dateGroup.end).format("h:mm a")}</h4>
             <div>
                 {dates}
             </div>
-            {props.dateForm(props.dateGroup.id)}
         </div>
     );
 }
+//{props.dateForm(props.dateGroup.id)}
 function Event(props){
-    if(!props.event.dateGroups){
+    if(false){
         return<div>not working</div>
     }
-    const dateGroups = props.event.dateGroups.map((element)=>{return props.child(element, element.id)});
+    const dateGroups = props.event.dateGroupsByEvent.edges.map((element)=>{return <DateGroup key={element.node.id} dateGroup={element.node}/>});
     return(
-        <div className="event-preview-event-container" >
-            <div className='event-preview-header'><h3>{props.event.name}</h3><a>edit</a></div>
+        <div onClick={()=>{props.setActiveEvent(props.event)}} className="event-preview-event-container" >
+            <div className='event-preview-header'><h3>{props.event.activityByEventType.name}</h3><a>edit</a></div>
             <div>
                 {dateGroups}
-                {props.dateGroupForm(props.event.id)}
+
             </div>
         </div>
     );
 }
-function EventsPreview(props) {
-    if(!props.events){
-        return<div></div>
-    }
-    const events = props.events.map((element)=>{return props.child(element, element.id)});
+//{props.dateGroupForm(props.event.id)}
+function EventsPreviewChild(props) {
+    console.log(props.queryResult);
+    const events = props.queryResult.allEvents.edges.map((element)=>{return <Event key={element.node.id} event={element.node}/>});
     return (
         <div className="event-preview-container-container">
             <div className="event-preview-container">
@@ -53,4 +56,55 @@ function EventsPreview(props) {
         </div>
     );
 }
+
+function EventsPreview(props){
+    const query = gql`{
+  allEvents {
+    edges {
+      node {
+        eventType
+        address
+        activityByEventType{
+          name
+        }
+        id
+        addressByAddress {
+          alias
+          id
+        }
+        openRegistration
+        closeRegistration
+        price
+        capacity
+        dateGroupsByEvent {
+          edges {
+            node {
+              openRegistration
+              closeRegistration
+              id
+              datesJoinsByDateGroup {
+                edges {
+                  node {
+                    id
+                    dateIntervalByDateInterval {
+                      start
+                      end
+                      id
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
+    return <QueryHandler query={query} child={(data) => {
+            return <EventsPreviewChild queryResult={data} {...props}/>
+        }}/>
+}
+
 export {Event, DateGroup, EventsPreview}
--- src/app/main/manageEvents/ManageEvents.js
+++ src/app/main/manageEvents/ManageEvents.js
@@ -5,7 +5,7 @@ import gql from 'graphql-tag';
 import {Query} from 'react-apollo';
 import DateTime from 'react-datetime';
 import '../../../react-datetime.css';
-import {Calendar, DragAndDropCalendar} from '../calendar/Calendar';
+import {Calendar, DragAndDropMutation} from '../calendar/Calendar';
 import QueryHandler from '../queryHandler/QueryHandler';
 import memoize from "memoize-one";
 import Colors from '../calendar/Colors';
@@ -52,13 +52,9 @@ class ManageEventsClass extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selected: {
-                id: null
-            },
             events: [],
-            calendarEvents: [],
-            currentEvent: null,
-            currentDateGroup: null
+            activeEvent: null,
+            activeDateGroup: null
         }
         this.events = [];
     }
@@ -84,7 +80,19 @@ class ManageEventsClass extends Component {
         })
     }
     setDateGroupStart = (groupId, start, end) =>{
-        console.log(groupId, start, end);
+        this.events = this.events.map((element) => {
+            element.dateGroups = element.dateGroups.map((dateGroup)=>{
+                if(dateGroup.id === groupId){
+                    dateGroup.start = start
+                    dateGroup.end = end;
+                }
+                return dateGroup;
+            });
+            return element;
+        });
+        this.setState({
+            foo:0
+        })
     }
     createDate = (date, groupId) => {
         this.events = this.events.map((element) => {
@@ -100,170 +108,30 @@ class ManageEventsClass extends Component {
             foo:0
         })
     }
-    newCalendarEvent = (event) => { //slot select
-        if(!this.state.currentDateGroup && !this.state.selected.id){
-            alert("Create a date group to begin");
-        }else if(!this.state.currentEvent && !this.state.selected.id){
-            alert("Create an event to begin");
-        }
-            if (event.action === 'doubleClick') {
-                let hour = {
-                    id: '_' + Math.random().toString(36).substr(2, 9),
-                    title: this.dateGroups.getName(this.state.currentEvent) +" " + this.state.currentEvent,
-                    start: event.start,
-                    end:  new Date(+event.start + 86400000),
-                    buttonStyle: {
-                        backgroundColor: Colors.get(this.state.currentDateGroup).regular
-                    },
-                    resources: {
-                        eventId: this.state.currentEvent,
-                        start: this.start,
-                        end: this.end,
-                        open: this.groupOpen,
-                        close: this.groupClose,
-                        groupId: this.state.currentDateGroup
-                    }
-                }
-                this.setState({
-                    calendarEvents: [
-                        ...this.state.calendarEvents,
-                        hour
-                    ]
-                })
-                return;
-            }
-          if(event.action==='select'){
-              let selectedId = this.state.selected.id;
-              if(selectedId != null){
-                const newEvents = this.state.calendarEvents.filter((element)=>{return element.id != selectedId});
-                let newEvent = Object.assign({}, this.state.selected);
-                newEvent.start = event.start;
-                newEvent.end = new Date(+event.start + 86400000 * event.slots.length);
-                this.setState({
-                    calendarEvents: [...newEvents, newEvent],
-                });
-            }
-        }
-          this.resetSelectedEvent();
-    }
-    removeCalendarEvent = (event) => {
-        const newCalendarEvents = this.state.calendarEvents.filter((element) => { //filter displayed events
-            return element.id != event.id;
-        });
-        let removeGroup = newCalendarEvents.filter((element) => { //check if the event was the last in the group
-            return element.resources.groupId == event.resources.groupId
-        }).length == 0;
-        this.setState({calendarEvents: newCalendarEvents})
-        if (removeGroup) { //if the event was the last in the group, remove the group from dateGroups
-            this.dateGroups.removeDateGroup(event.resources.groupId)
-        }
-    }
-    selectEvent = (event) => {
-        this.resetSelectedEvent();
-        event.buttonStyle = {
-            backgroundColor: Colors.get(event.resources.groupId).hover
-        }
-        this.setState({selected: event})
-    }
-    resetSelectedEvent = () => {
+    setActiveDateGroup = (dateGroup, event) =>{
         this.setState({
-            selected: {
-                id: null
-            },
-            calendarEvents: this.state.calendarEvents.map((element) => {
-                element.buttonStyle = {
-                    backgroundColor: Colors.get(element.resources.groupId).regular
-                }
-                return element;
-            })
+            activeEvent:event,
+            activeDateGroup:dateGroup
         });
     }
-    submitEvents = () =>{
-        function spreadDates (eventStart, eventEnd, dayStart, dayEnd){
-            let dates = []
-            let startTime = new Date(dayStart);
-            let endTime = new Date(dayEnd);
-            let start = new Date(eventStart);
-            let date1;
-            let date2;
-            while(start < eventEnd){
-                date1 = new Date(start);
-                date2 = new Date(start);
-                date1.setHours(startTime.getHours(), startTime.getMinutes(), startTime.getSeconds());
-                date2.setHours(endTime.getHours(), endTime.getMinutes(), endTime.getSeconds());
-                dates.push({
-                    start:date1,
-                    end:date2
-                })
-                start = moment(start).add(1, 'days');
-            }
-            return dates;
-        }
-        let events = Object.keys(this.dateGroups.data).map((key)=>{
-            let dateGroup = this.dateGroups.data[key].dateGroups.map((groupId)=>{
-                let open;
-                let close;
-                let calendarEvent = this.state.calendarEvents.filter((event)=>{
-                    return event.resources.groupId == groupId;
-                }).map((event)=>{
-                    open=event.resources.open;
-                    close=event.resources.close;
-                    return spreadDates(event.start, event.end, event.resources.start, event.resources.end)
-                })
-                return {
-                    open: open,
-                    close: close,
-                    dates: calendarEvent.reduce((acc, val) => acc.concat(val), [])
-                }
-            }).filter((group)=>{
-                return group.open && group.close && group.dates.length != 0;
-            })
-            return {
-                dateGroups: dateGroup,
-                open: this.dateGroups.data[key].open,
-                close: this.dateGroups.data[key].close,
-                type: this.dateGroups.data[key].type,
-                address: this.dateGroups.data[key].address,
-                capacity: this.dateGroups.data[key].capacity
-            };
+    setActiveEvent = (event) => {
+        this.setState({
+            activeEvent: event
         });
-        console.log(events);
     }
-    tooltipAccessor = (event) => {
-        let group = this.dateGroups.data[event.resources.eventId];
-        return `From: ${moment(event.resources.start).format("h:mm a")}
-To: ${moment(event.resources.end).format("h:mm a")}
-Price: ${group.price}
-Location: ${group.addressName}
-Capacity: ${group.capacity}
-Registartion
-    Event
-        Open: ${moment(group.open).format("MMMM Do YYYY")} Close: ${moment(group.close).format("MMMM Do YYYY")}
-    Group:
-        Open: ${moment(event.resources.open).format("MMMM Do YYYY")} Close: ${moment(event.resources.close).format("MMMM Do YYYY")}
-Event Id:${event.resources.eventId}
-`;
-    }
+
     render() {
-        const dateForm = groupId => <DateForm groupId={groupId} setDateGroupStart={this.setDateGroupStart} />
-        const dateGroupForm = eventId => <DateGroupForm eventId={eventId} createDateGroup={this.createDateGroup} events={this.events}/>
-        const dateGroup = (dateGroup, key) => <DateGroup dateForm={dateForm} key={key} dateGroup={dateGroup} />
-        const event = (event, key) => <Event key={key} child={dateGroup} dateGroupForm={dateGroupForm} event={event} />
         return (
             <div className="manage-events-container">
-                <EventsPreview child={event} events={this.events}/>
+                <EventsPreview/>
                 <div className="manage-events-main">
                     <div className="manage-events-event-form">
                         <EventForm createEvent={this.createEvent}/>
+                        {JSON.stringify(this.state.activeDateGroup)}
                     </div>
-                        <DragAndDropCalendar
-                        tooltipAccessor={this.tooltipAccessor}
-                        selected={this.state.selected}
-                        removeEvent={this.removeCalendarEvent}
-                        selectEvent={this.selectEvent}
-                        newEvent={this.newCalendarEvent}
-                        events={this.state.calendarEvents}
-                        className="manage-events-calander"/>
+                        <DragAndDropMutation
+                            createDate={this.state.createDate}
+                            activeDateGroup={this.state.activeDateGroup}/>
                 </div>
         </div>
     );
