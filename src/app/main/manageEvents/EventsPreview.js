9884190e7d01dd4b4e5c50360012bfa72e0bb051
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-11-27 13:53:01 -0600

    implement edit event feature

--- src/app/main/manageEvents/EventsPreview.js
+++ src/app/main/manageEvents/EventsPreview.js
@@ -6,19 +6,22 @@ import QueryHandler from '../queryHandler/QueryHandler'
 import DateTime from 'react-datetime'
 import '../../../react-datetime.css'
 import memoize from 'memoize-one'
+import Popup from "reactjs-popup"
 import Colors from '../calendar/Colors'
 import moment from 'moment'
 import DateStore from '../../DateStore'
+import EventForm from './EventForm';
 
 function DateGroup (props) {
   if (false) {
     return <div>not working</div>
   }
-  const dates = props.dateGroup.datesJoinsByDateGroup.nodes.map((element) => {
-    return <div key={element.id}>{element.dateIntervalByDateInterval.start}</div>
+  let dates = props.dateGroup.datesJoinsByDateGroup.nodes.slice().sort((a,b)=>{return moment(a.dateIntervalByDateInterval.start).unix() - moment(b.dateIntervalByDateInterval.start).unix()})
+  dates = dates.map((element) => {
+    return <div key={element.id}>{moment(moment.utc(element.dateIntervalByDateInterval.start)).local().toString()}</div>
   })
   return (
-    <div onClick={() => { props.setActiveDateGroup(props.dateGroup)}} style={{ backgroundColor: Colors.get(props.dateGroup.id).regular }} className='event-preview-date-container'>
+    <div onClick={() => {props.setActiveDateGroup(props.dateGroup)}} style={{ backgroundColor: Colors.get(props.dateGroup.id).regular }} className='event-preview-date-container'>
       <h4>{props.dateGroup.name}</h4>
       <h4> Show on Calander <input onChange={() => { DateStore.set('toggleDateDisplay', props.dateGroup.id) }} type='checkbox' defaultChecked='true' /> </h4>
       <div>
@@ -28,20 +31,50 @@ function DateGroup (props) {
   )
 }
 // {props.dateForm(props.dateGroup.id)}
-function Event (props) {
-  if (false) {
-    return <div>not working</div>
-  }
-  const dateGroups = props.event.dateGroupsByEvent.nodes.map((element) => { return React.cloneElement(props.children, { key: element.id, dateGroup: element }) })
-  return (
-    <div className='event-preview-event-container'>
-      <div className='event-preview-header'><h3>{props.event.activityByEventType.name}</h3><a>edit</a></div>
-      <div>
-        {dateGroups}
-        {React.cloneElement(props.form, { eventId: props.event.id })}
-      </div>
-    </div>
-  )
+class Event extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            showPopup: false
+        }
+    }
+    showPopup = () =>{
+        this.setState({showPopup:true});
+    }
+    clearPopupState = () =>{
+        this.setState({showPopup:false});
+    }
+    render = () => {
+        if (false) {
+            return <div>not working</div>
+        }
+        const event = this.props.event;
+        const dateGroups = event.dateGroupsByEvent.nodes.map((element) => { return React.cloneElement(this.props.children, { key: element.id, dateGroup: element }) })
+        return (
+            <React.Fragment>
+                <Popup
+                open={this.state.showPopup}
+                closeOnDocumentClick
+                onClose={this.clearPopupState}>
+                    <EventForm id={event.id}
+                        nodeId={event.nodeId}
+                        openRegistration={event.openRegistration}
+                        closeRegistration={event.closeRegistration}
+                        price={event.price}
+                        capacity={event.capacity}
+                        address={event.addressByAddress.id}
+                        eventType={event.activityByEventType.id}/>
+                </Popup>
+                <div className='event-preview-event-container'>
+                    <div className='event-preview-header'><h3>{event.activityByEventType.name}</h3><a onClick={this.showPopup}>edit</a></div>
+                    <div>
+                        {dateGroups}
+                        {React.cloneElement(this.props.form, { eventId: this.props.event.id })}
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
 }
 //
 function EventsPreviewChild (props) {
@@ -63,7 +96,7 @@ function DateGroupInfo(props){
     if(props.activeDateGroup === null){
         return <div></div>
     }
-    const child  = (dateGroup)=> { console.log(dateGroup)
+    const child  = (dateGroup)=> {
         let event = dateGroup.eventByEvent;
         return <React.Fragment>
             <table>
