1e79fac245f0d315d070b48fdf79ca71c9727f8f
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-01-16 16:55:47 -0600

    improve order history formatting

--- src/app/main/orderHistory/OrderHistory.js
+++ src/app/main/orderHistory/OrderHistory.js
@@ -3,6 +3,8 @@ import Mutation from '../../../delv/Mutation'
 import {ReactQuery} from '../../../delv/delv-react'
 import {SecureRoute} from '../common/Common'
 import moment from 'moment'
+import './OrderHistory.css'
+
 const USER_DATA = id => `{
 	allPayments(condition:{userId:"${id}"}, orderBy:CREATE_ON_DESC){
     nodes{
@@ -16,16 +18,38 @@ const USER_DATA = id => `{
 }`
 
 function Order(props){
-    return <div>
-        <h3 className='no-margin'>{props.payment.snapshot.dateGroup.name}</h3>
-        <div>{moment(props.payment.createOn).format('MMMM, Do YYYY h:mm a')} Order number:{props.payment.id}</div>
-        {JSON.stringify(props.payment.snapshot)}
+	const students = props.payment.snapshot.students.map((student)=>{
+		return <div key={student.id}>
+			{student.first_name} {student.last_name}
+			<div className='margin-left-30'>
+			{props.payment.snapshot.addons.map(addon=><div key={addon.id}>{addon.name}</div>)}
+			</div>
+		</div>
+	})
+    return <div className='margin-left-30 container column'>
+		<div className='order-header'>
+			<div><h3 className='no-margin'>{moment(props.payment.createOn).format('MMMM, Do YYYY')}</h3></div>
+			<div><span>Order # {props.payment.id}</span></div>
+		</div>
+		<div className='container section'>
+			<div className='padding-10 section'>
+				<h4 className='no-margin'>{props.payment.snapshot.activity.name}</h4>
+				<div className='margin-left-30'>
+					{students}
+				</div>
+			</div>
+			<div className='container column padding-10'>
+				<button className='order-btn'>Event Details</button>
+				<button className='order-btn'>Order Details</button>
+				<button className='order-btn'>Request a refund</button>
+			</div>
+		</div>
     </div>
 }
 
 function OrderHistoryInner(props){
     const orders = props.allPayments.nodes.map((payment) => { return <Order key={payment.nodeId} payment={payment}/>})
-    return <div>{orders}</div>
+    return <React.Fragment>{orders}</React.Fragment>
 }
 
 function OrderHistory(props){
