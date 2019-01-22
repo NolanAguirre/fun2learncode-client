8bdcf387d86556950c542306e2a01d32de9cd253
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-01-21 22:45:45 -0600

    add page for no classes offered

--- src/app/main/activities/Activities.js
+++ src/app/main/activities/Activities.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import './Activities.css'
 import Activity from './activity/Activity'
 import {ReactQuery} from '../../../delv/delv-react'
-
+import SadFace from '../../logos/sadface.svg'
 const GET_ACTIVITIES_IN_CATAGORY = (name) => {
     return `{
    allActivityCatagories(condition: {name: "${name}", publicDisplay:true}) {
@@ -38,21 +38,23 @@ const GET_ACTIVITIES_IN_CATAGORY = (name) => {
 function ActivitiesInner(props) {
     const activities = props.allActivityCatagories.nodes[0].activitiesByType.nodes;
     if (activities.length == 0) {
-        return <div>We currently aren't offering any {props.allActivityCatagories.nodes[0].name.toLowerCase()}.</div>
+        return <div style={{background:'white'}}className='center-y section'>
+            <img src={SadFace} title='Icon made by Freepik from www.flaticon.com' />
+            <h2 style={{color:'rgb(164, 164, 164)'}} className='center-text'>Sorry! We currently aren't offering any {props.catagory.toLowerCase()} at this time.</h2>
+        </div>
     }
-    return activities.map((element) => {
-        let prerequisites = element.activityPrerequisitesByActivity.nodes.map((prerequisite) => prerequisite.activityByPrerequisite.name)
+    return <div className='container column section'>
+        <h1 className='center-text'>{props.catagory}</h1>
+        {activities.map((element) => {
+        let prerequisites = element.activityPrerequisitesByActivity.nodes.map(prerequisite => prerequisite.activityByPrerequisite.name)
         return <Activity name={element.name} prerequisites={prerequisites} description={element.description} id={element.id} key={element.id} url={element.url}/>
-    })
+    })}
+</div>
 }
 
 function Activities(props) {
-    return <div className='container column section'>
-        <h1 className='center-text'>{props.match.params.type}</h1>
-        <ReactQuery query={GET_ACTIVITIES_IN_CATAGORY(props.match.params.type)}>
-            <ActivitiesInner/>
+    return  <ReactQuery query={GET_ACTIVITIES_IN_CATAGORY(props.match.params.type)}>
+            <ActivitiesInner catagory={props.match.params.type}/>
         </ReactQuery>
-    </div>
-
 }
 export default Activities
