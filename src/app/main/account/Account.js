8cb06fe7385b7accc669b694b99c4dda7795d04b
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-02-02 01:52:56 -0600

    add update email form

--- src/app/main/account/Account.js
+++ src/app/main/account/Account.js
@@ -11,6 +11,91 @@ const RESET_PASSWORD = `mutation($password:String!){
     clientMutationId
   }
 }`
+const UPDATE_EMAIl = (id) => `mutation ($email: String!) {
+  updateUserById(input: {userPatch: {email: $email}, id: "${id}"}) {
+    user {
+      email
+      id
+      nodeId
+    }
+  }
+}
+`
+
+
+class UpdateEmail extends Component{
+    constructor(props){
+        super(props);
+        this.state = {email:''}
+        this.mutation = new Mutation({
+            mutation:this.props.mutation,
+            onSubmit:this.handleSubmit,
+            onResolve:this.handleResolve,
+            customCache: this.customCache
+        })
+    }
+    customCache = (cache, data) => {
+        const {
+            updateUserById: {
+                user:{
+                    nodeId,
+                    email
+                }
+            }
+        } = data
+        cache.cache.User[nodeId].email = email
+        cache.emitter.changeType('User')
+        cache.emitter.emitCacheUpdate()
+    }
+    handleChange = (event) => {
+      const target = event.target
+      const value = target.value
+      const name = target.name
+      this.setState({
+        [name]: value,
+        error:undefined
+      })
+    }
+
+    validEmail = () => {
+        return this.state.email.match('^.+@.+\..+$');
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        if(!this.validEmail()){
+            this.setState({error:'Email is not valid.'})
+            return false;
+        }
+        return {email:this.state.email}
+    }
+    handleResolve = (data) => {
+        if(data.errors){
+           this.setState({error:"Email change failed for unknown reason."})
+        }else{
+           this.setState({complete:true})
+        }
+    }
+
+    render = () => {
+        if(this.state.complete){
+            return <div className='center-y section'>
+                    <div className='center-text'>Email has been changed.</div>
+                </div>
+        }else{
+            return <form onSubmit={this.mutation.onSubmit}>
+                <div className="email-input-container">
+                    <div className='error'>{this.state.error}</div>
+                    <input className='styled-input' name='email' type='email' onChange={this.handleChange} placeholder='email' />
+                </div>
+                <div className="email-input-container">
+                    <div className='event-register-btn center-text' onClick={this.mutation.onSubmit}>Change email</div>
+                </div>
+                <button className='hacky-submit-button' type='submit'/>
+            </form>
+        }
+    }
+}
 
 class AccountInner extends Component{
     constructor(props){
@@ -33,6 +118,15 @@ class AccountInner extends Component{
                             <tr>
                                 <td>Email: </td>
                                 <td>{user.email}</td>
+                                <td>
+                                    <BasicPopup className="login-widget">
+                                        <div className='space-around'>
+                                            <h2 className='center-text no-margin'>Change email</h2>
+                                            <UpdateEmail mutation={UPDATE_EMAIl(user.id)}/>
+                                        </div>
+                                        <div style={{color:'blue'}}>Change email</div>
+                                    </BasicPopup>
+                                </td>
                             </tr>
                             <tr>
                                 <td>Memeber since: </td>
