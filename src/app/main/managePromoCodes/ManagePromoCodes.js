fc0fd526b623aa51329be7873fcde8fc6344daf0
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-02-04 14:27:46 -0600

    reformat and finsih manage promo codes

--- src/app/main/managePromoCodes/ManagePromoCodes.js
+++ src/app/main/managePromoCodes/ManagePromoCodes.js
@@ -4,6 +4,7 @@ import DateTime from 'react-datetime';
 import { ReactQuery } from '../../../delv/delv-react'
 import Mutation from '../../../delv/Mutation'
 import moment from 'moment'
+import './ManagePromoCodes.css'
 const GET_DROPDOWN = `{
     allActivityCatagories {
       nodes {
@@ -35,6 +36,8 @@ const GET_PROMO_CODES =  (archive) =>`{
       effect
       uses
       code
+      validStart
+      validEnd
       userByUserId {
         nodeId
         id
@@ -64,10 +67,48 @@ const GET_PROMO_CODES =  (archive) =>`{
 const CREATE_PROMO_CODE = `mutation($promoCode:PromoCodeInput!){
   createPromoCode(input:{promoCode:$promoCode}){
     promoCode{
-      nodeId
       id
+      nodeId
       code
+      effect
       uses
+      code
+      validStart
+      validEnd
+      userByUserId {
+        nodeId
+      }
+      activityCatagoryByCatagory {
+        nodeId
+      }
+      activityByActivity {
+        nodeId
+      }
+      forUser
+      forActivity
+      forCatagory
+      percent
+      archive
+      disabled
+    }
+  }
+}`
+
+const UPDATE_PROMO_CODE = `mutation($archive:Boolean, $disable:Boolean, $id:UUID!){
+  updatePromoCodeById(input:{promoCodePatch:{archive:$archive,disabled:$disable},id:$id}){
+    promoCode{
+      nodeId
+      archive
+      disabled
+      userByUserId {
+        nodeId
+      }
+      activityCatagoryByCatagory {
+        nodeId
+      }
+      activityByActivity {
+        nodeId
+      }
     }
   }
 }`
@@ -166,56 +207,24 @@ class CreatePromoCode extends Component{
     }
     render = () => {
         const types = this.props.allActivityCatagories.nodes.map((element) =>  {return{name: element.name, value: element.id}})
-        return <form onSubmit={this.mutation.onSubmit}>
-            <table>
-                <tbody>
-                    <tr>
-                        <td>For specific catagory</td>
-                        <td><input checked={this.state.forCatagory} name='forCatagory' onChange={this.handleChange} type='checkbox'></input></td>
-                        {this.state.forCatagory?<td>Catagory: <DropDown options={types} name="catagory" value={this.state.catagory} onChange={this.handleChange}/></td>:<td></td>}
-                    </tr>
-                    <tr>
-                        <td>For specific activity</td>
-                        <td><input checked={this.state.forActivity} name='forActivity' onChange={this.handleChange} type='checkbox'></input></td>
-                        {this.state.forActivity?<td>Activity: <EventDropDownInner name='activity' value={this.state.activity} allActivities={this.props.allActivities} onChange={this.handleChange}/></td>:<td></td>}
-                    </tr>
-                    <tr>
-                        <td>For specific user</td>
-                        <td><input checked={this.state.forUser} name='forUser' onChange={this.handleChange} type='checkbox'></input></td>
-                        {this.state.forUser?<td>user id: <input value={this.state.userId} name='userId' onChange={this.handleChange}></input></td>:<td></td>}
-                    </tr>
-                    <tr>
-                        <td>Percent Reduction</td>
-                        <td><input checked={this.state.percent} name='percent' onChange={this.handleChange} type='checkbox'></input></td>
-                        <td>Amount: <input type='number'></input> {this.state.percent?"% off":"$ off"}</td>
-                    </tr>
-                    <tr>
-                        <td>Uses</td>
-                        <td></td>
-                        <td><input name='uses' onChange={this.handleChange} value={this.state.uses} type='number'></input></td>
-                    </tr>
-                    <tr>
-                        <td>Start Date:</td>
-                        <td></td>
-                        <td><DateTime dateFormat="MMMM Do YYYY" value={this.state.validStart} timeFormat={false} onChange={(time) =>{this.handleTimeChange("validStart", time)}}/></td>
-                    </tr>
-                    <tr>
-                        <td>End Date:</td>
-                        <td></td>
-                        <td><DateTime dateFormat="MMMM Do YYYY" value={this.state.validEnd} timeFormat={false} onChange={(time) =>{this.handleTimeChange("validEnd", time)}}/></td>
-                    </tr>
-                    <tr>
-                        <td>Code random</td>
-                        <td><input value={this.state.isRandom} name='isRandom' onChange={this.handleChange} type='checkbox'></input></td>
-                    </tr>
-                    <tr>
-                        <td>Code</td>
-                        <td></td>
-                        <td><input value={this.state.code} name='code' onChange={this.handleChange}></input></td>
-                    </tr>
-                </tbody>
-            </table>
-            <div className='event-register-btn center-text' onClick={this.mutation.onSubmit}>Create</div>
+        return <form className='styled-container column' onSubmit={this.mutation.onSubmit}>
+            <div className='section container center-x'>
+                <div className='promo-form-column'>
+                    <div>For catagory <input checked={this.state.forCatagory} name='forCatagory' onChange={this.handleChange} type='checkbox'/>{this.state.forCatagory?<DropDown options={types} name="catagory" value={this.state.catagory} onChange={this.handleChange}/>:''}</div>
+                    <div>For activity <input checked={this.state.forActivity} name='forActivity' onChange={this.handleChange} type='checkbox'/>{this.state.forActivity?<EventDropDownInner name='activity' value={this.state.activity} allActivities={this.props.allActivities} onChange={this.handleChange}/>:''}</div>
+                    <div>For user <input checked={this.state.forUser} name='forUser' onChange={this.handleChange} type='checkbox'/>{this.state.forUser?<React.Fragment>id: <input value={this.state.userId} name='userId' onChange={this.handleChange}/></React.Fragment>:''}</div>
+                    <div>Percent Reduction <input checked={this.state.percent} name='percent' onChange={this.handleChange} type='checkbox'/></div>
+                    <div>Amount: <input value={this.state.effect} name='effect' type='number' onChange={this.handleChange} style={{width:'30px'}}/> {this.state.percent?"% off":"$ off"}</div>
+                </div>
+                <div className='promo-form-column'>
+                    <div className='container'>Start Date: <DateTime style={{width:'150px'}} dateFormat="MMMM Do YYYY" value={this.state.validStart} timeFormat={false} onChange={(time) =>{this.handleTimeChange("validStart", time)}}/></div>
+                    <div className='container'>End Date: <DateTime className='tiny-input' dateFormat="MMMM Do YYYY" value={this.state.validEnd} timeFormat={false} onChange={(time) =>{this.handleTimeChange("validEnd", time)}}/></div>
+                    <div>Code random <input value={this.state.isRandom} name='isRandom' onChange={this.handleChange} type='checkbox'/></div>
+                    <div>Code <input value={this.state.code} name='code' onChange={this.handleChange} style={{width:'100px'}}/></div>
+                    <div>Uses <input name='uses' onChange={this.handleChange} value={this.state.uses} type='number' style={{width:'30px'}}/></div>
+                </div>
+            </div>
+            <div className='event-register-btn center-text center-x' style={{width:'200px'}}onClick={this.mutation.onSubmit}>Create</div>
             <button className='hacky-submit-button' type='submit'/>
         </form>
     }
@@ -226,15 +235,49 @@ class DisablePromoCode extends Component{
         super(props);
         this.state = {
             archive:this.props.archive,
-            disabled: this.props.disabled
+            disabled:this.props.disabled
         }
+        this.mutation = new Mutation({
+            mutation:UPDATE_PROMO_CODE,
+            onSubmit:this.handleSubmit
+        })
+    }
+    handleChange = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox'
+            ? target.checked
+            : target.value;
+        const name = target.name;
+        this.setState({[name]: value});
+    }
+    hasRequiredValues = () =>{
+        return this.state.archive != this.props.archive ||
+            this.state.disabled != this.props.disabled
+    }
+    handleSubmit = (event) => {
+        event.preventDefault()
+        if(this.hasRequiredValues()){
+            return {...this.state, id:this.props.id}
+        }
+        return false;
+    }
+    getFor = () => {
+        if(this.props.forCatagory){
+            return this.props.activityCatagoryByCatagory.name
+        }else if(this.props.forActivity){
+            return this.props.activityByActivity.name
+        }else if(this.props.forUser){
+            const user = this.props.userByUserId
+            return `${user.firstName} ${user.lastName}`
+        }
+        return 'All'
     }
     render = () => {
-        return <div>
+        return <form className='manage-address-form-container'>
             <table>
                 <tbody>
                     <tr>
-                        <td>Code</td>
+                        <td>Code:</td>
                         <td>{this.props.code}</td>
                     </tr>
                     <tr>
@@ -242,16 +285,39 @@ class DisablePromoCode extends Component{
                         <td>{this.props.uses}</td>
                     </tr>
                     <tr>
-                        <td>effect</td>
+                        <td>effect:</td>
                         <td>{this.props.effect}{this.props.percent?'%':'$'} off</td>
+                    </tr>
+                    <tr>
+                        <td>For:</td>
+                        <td>{this.getFor()}</td>
+                    </tr>
+                    <tr>
+                        <td>Start:</td>
+                        <td>{moment(this.props.validStart).format('MMM Do YYYY')}</td>
+                    </tr>
+                    <tr>
+                        <td>End:</td>
+                        <td>{moment(this.props.validEnd).format('MMM Do YYYY')}</td>
+                    </tr>
+                    <tr>
+                        <td>Archive:</td>
+                        <td><input checked={this.state.archive} name='archive' onChange={this.handleChange} type='checkbox'/></td>
+                    </tr>
+                    <tr>
+                        <td>Disable:</td>
+                        <td><input checked={this.state.disabled} name='disabled' onChange={this.handleChange} type='checkbox'/></td>
                     </tr>
                 </tbody>
             </table>
-        </div>
+            <div className='event-register-btn center-text' onClick={this.mutation.onSubmit}>update</div>
+            <button className='hacky-submit-button' type='submit'/>
+        </form>
     }
 }
+
 function ViewPromoCodes(props){
-    return <GridView itemsPerRow={5}>
+    return <GridView itemsPerRow={4}>
         {props.allPromoCodes.nodes.map(promoCode=><DisablePromoCode key={promoCode.nodeId}{...promoCode}/>)}
     </GridView>
 }
@@ -275,9 +341,11 @@ class ManagePromoCodesInner extends Component {
 
 function ManagePromoCodes(props){
     return <SecureRoute ignoreResult roles={["FTLC_LEAD_INSTRUCTOR", "FTLC_OWNER", "FTLC_ADMIN"]}>
+        <div className='section container column'>
             <ReactQuery query={GET_DROPDOWN}>
-                <ManagePromoCodesInner />
+                    <ManagePromoCodesInner />
             </ReactQuery>
+        </div>
         </SecureRoute>
 }
 export default ManagePromoCodes;
