6c935f497afce0114284ae19b8a70097f97d49aa
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-02-12 16:25:35 -0500

    add query full event component

--- src/app/main/events/event/QueryFullEvent.js
+++ src/app/main/events/event/QueryFullEvent.js
@@ -2,21 +2,37 @@ import React, { Component } from 'react'
 import './Event.css'
 import {DatesTable, CustomProvider} from '../../common/Common'
 import {ReactQuery} from '../../../../delv/delv-react'
+import moment from 'moment'
+
 const GET_EVENT = (id) => `{
-  allEvents(condition:{id:"${id}"}){
-    nodes{
-    	id
+  allEvents(condition: {id: "${id}"}) {
+    nodes {
+      id
       nodeId
       price
       openRegistration
       closeRegistration
       capacity
       name
-      activityByActivity{
+      seatsLeft
+      addOnJoinsByEvent{
+        nodes{
+          nodeId
+          id
+          addOnByAddOn{
+            nodeId
+            id
+            price
+            name
+            description
+          }
+        }
+      }
+      activityByActivity {
         nodeId
         name
       }
-      addressByAddress{
+      addressByAddress {
         nodeId
         alias
         street
@@ -24,47 +40,13 @@ const GET_EVENT = (id) => `{
         city
         zipcode
       }
-      eventRegistrationsByEvent{
-        nodes{
+      dateJoinsByEvent {
+        nodes {
           nodeId
-          id
-          registeredOn
-          userByRegisteredBy{
-            nodeId
-            firstName
-            lastName
-          }
-          studentByStudent{
-            nodeId
-            firstName
-            lastName
-          }
-        }
-      }
-      dateJoinsByEvent{
-        nodes{
-          nodeId
-          dateIntervalByDateInterval{
+          dateIntervalByDateInterval {
             nodeId
             start
             end
-            eventLogsByDateInterval(condition:{event:"${id}"}){
-              nodes{
-                nodeId
-                event
-                comment
-                userByInstructor{
-                  nodeId
-                  firstName
-                  lastName
-                }
-                studentByStudent{
-                  nodeId
-                  firstName
-                  lastName
-                }
-              }
-            }
           }
         }
       }
@@ -72,23 +54,66 @@ const GET_EVENT = (id) => `{
   }
 }`
 
+function localizeUTCTimestamp(timestamp){
+    return moment(moment.utc(timestamp)).local()
+}
+
 function FullEventInner(props){
     const event = props.allEvents.nodes[0]
     if(!event){
         return <div>No event selected</div>
     }
     const address = event.addressByAddress
-    const registrations = event.eventRegistrationsByEvent
+    const addons = event.addOnJoinsByEvent.nodes.map((a)=>{
+        return <div key={a.id} className='query-event-addon-container'>
+              <h3>{a.addOnByAddOn.name}</h3>
+              <div className='addon-container-description'>
+                  {a.addOnByAddOn.description}
+              </div>
+              <div>
+                  {a.addOnByAddOn.price}$
+              </div>
+        </div>
+    })
     return <div className='container column'>
-        <div>Activity: {event.activityByActivity.name}</div>
-        <div>Event name:{event.name}</div>
-        <DatesTable dates={event.dateJoinsByEvent}/>
-        {JSON.stringify(props.allEvents)}
+        <h1>Event info</h1>
+        <table>
+            <tbody>
+                <tr>
+                    <td>Activity:</td>
+                    <td>{event.activityByActivity.name}</td>
+                    <td>Event name:</td>
+                    <td>{event.name}</td>
+                </tr>
+                <tr>
+                    <td>Price:</td>
+                    <td>{event.price}</td>
+                    <td>Seats left:</td>
+                    <td>{event.seatsLeft} of {event.capacity}</td>
+                </tr>
+                <tr>
+                    <td>Open:</td>
+                    <td>{localizeUTCTimestamp(event.openRegistration).format('MMM Do YYYY h:mm a')}</td>
+                    <td>Close:</td>
+                    <td>{localizeUTCTimestamp(event.closeRegistration).format('MMM Do YYYY h:mm a')}</td>
+                </tr>
+            </tbody>
+        </table>
+        <div className='query-event-date-container'>
+            <DatesTable dates={event.dateJoinsByEvent}/>
+              <div>
+                <h3 className='center-text'>{address.alias}</h3>
+                <div>{address.street}, {address.city} {address.state}</div>
+              </div>
+        </div>
+        {(addons.length > 0)?<h2>Available Add-ons</h2>:''}
+        <div className='registration-addons-container'>
+            {addons}
+        </div>
     </div>
 }
 
 function InBetween(props){
-    console.log(props.eventId || props.activeEventProvider.id)
     return <ReactQuery query={GET_EVENT(props.eventId || props.activeEventProvider.id)}>
         <FullEventInner />
     </ReactQuery>
