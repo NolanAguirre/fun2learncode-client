a97b6f6ffac4d79169afbe3092ab0a7587f43f19
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-02-20 00:13:15 -0600

    refactor recent event to group by student

--- src/app/main/recentEvents/RecentEvents.js
+++ src/app/main/recentEvents/RecentEvents.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import moment from 'moment'
+import './RecentEvent.css'
 import {ReactQuery} from '../../../delv/delv-react'
-import {TimeRangeSelector, SecureRoute} from '../common/Common'
+import {TimeRangeSelector, SecureRoute, GridView} from '../common/Common'
 import Popup from "reactjs-popup"
+import {Link} from 'react-router-dom'
 
 const RECENT_EVENT = (start, end) => `{
   eventInDates(arg0: "${start}", arg1: "${end}") {
@@ -11,6 +13,7 @@ const RECENT_EVENT = (start, end) => `{
       name
       seatsLeft
       capacity
+      closeRegistration
       activityByActivity{
         id
         name
@@ -23,6 +26,10 @@ const EVENT_REGISTRATIONS = (id) => `{
   allEvents(condition: {id: "${id}"}) {
     nodes {
       id
+      activityByActivity{
+        id
+        name
+      }
       dateJoinsByEvent {
         nodes {
           id
@@ -40,18 +47,6 @@ const EVENT_REGISTRATIONS = (id) => `{
             id
             firstName
             lastName
-            eventLogsByStudent(condition: {event: "${id}"}) {
-              nodes {
-                event
-                id
-                dateInterval
-                comment
-                userByInstructor {
-                  firstName
-                  lastName
-                }
-              }
-            }
             attendancesByStudent(condition: {event: "${id}"}) {
               nodes {
                 id
@@ -67,64 +62,78 @@ const EVENT_REGISTRATIONS = (id) => `{
   }
 }`
 
-class RecentEventStudent extends Component{
+const localize = (timestamp) =>{
+    return moment(moment.utc(timestamp)).local()
+}
+
+function RecentEventStudent(props){
+    let checkIn = (props.attendance && localize(props.attendance.checkInTime).format('h:mm a')) || 'None found.'
+    return <div>
+        <h3 className='no-margin'>{props.firstName} {props.lastName}</h3>
+        <div>Check in: {checkIn}</div>
+        <Link to={`/Logs/${props.eventId}/${props.id}`}>View/Write logs</Link>
+    </div>
+}
+class RecentEventDay extends Component{
     constructor(props){
         super(props)
         this.state = {}
     }
     render = () => {
-        //date header
-        //student container
-        //student name, view/write logs thing, attendance
-
-        console.log(this.props)
-        return <div>
-            <h2>{moment(this.props.dateIntervalByDateInterval.start).format('dddd')}</h2>
-            <span>{moment(this.props.dateIntervalByDateInterval.start).format('h:mm a')} - {moment(this.props.dateIntervalByDateInterval.end).format('h:mm a')}</span>
-
+        return <div className='recent-event-day-container'>
+            <h2>{localize(this.props.start).format('dddd MMMM Do')}</h2>
+            <span>{localize(this.props.start).format('h:mm a')} - {localize(this.props.end).format('h:mm a')}</span>
+            <GridView>
+                {this.props.students.map((student)=><RecentEventStudent {...student} eventId={this.props.eventId} key={student.id}/>)}
+            </GridView>
         </div>
     }
 }
 
-class RecentEventsStudents extends Component{
+class RecentEventDays extends Component{
     constructor(props){
         super(props)
         this.state = {}
     }
     render = () => {
         const event = this.props.allEvents.nodes[0]
-        let dates = event.dateJoinsByEvent.nodes.map(date=>{return{...date, logs:[], attendances:[]}})
+        let dates = event.dateJoinsByEvent.nodes.map(date=>{return{...date.dateIntervalByDateInterval, students:[]}}).sort((a,b)=>a.start < b.start)
         if(dates){
             event.eventRegistrationsByEvent.nodes.forEach((reg)=>{
-                const student = {
-                    id:reg.studentByStudent.id,
-                    firstName:reg.studentByStudent.firstName,
-                    lastName:reg.studentByStudent.lastName
-                }
-                reg.studentByStudent.attendancesByStudent.nodes.forEach((att)=>{
-                    dates.filter((date)=>{date.id === att.dateInterval})[0].attendances.push({...att, student})
+                dates.forEach((date)=>{
+                    date.students.push({
+                        id:reg.studentByStudent.id,
+                        firstName:reg.studentByStudent.firstName,
+                        lastName:reg.studentByStudent.lastName,
+                    })
                 })
-
-                reg.studentByStudent.eventLogsByStudent.nodes.forEach((log)=>{
-                    dates.filter((date)=>{date.id === log.dateInterval})[0].logs.push({...log, student})
+                reg.studentByStudent.attendancesByStudent.nodes.forEach((att)=>{
+                    let date = dates.filter((date)=>date.id === att.dateInterval)[0]
+                    if(date){
+                        date.students.filter((s)=>reg.studentByStudent.id === s.id)[0].attendance = att
+                    }
                 })
             })
         }
+        console.log(dates)
         return <div>
             {dates.map((date)=>{
-                return <RecentEventStudent key={date.id} {...date}/>
+                return <RecentEventDay key={date.id} {...date} eventId={event.id}/>
             })}
-            {JSON.stringify(dates)}
+        </div>
+        return <div>
+            No Dates planned for this event
         </div>
     }
 }
 
 function AdminEvent(props){
-    return <div>
+    return <div className='recent-event'>
         <div>
             <h1 className='no-margin'>{props.event.activityByActivity.name}</h1>
             <h2 className='no-margin'>{props.event.name}</h2>
-            Registration: {props.event.seatsLeft} of {props.event.capacity}
+            Registration: {props.event.seatsLeft} of {props.event.capacity} <br/>
+            Close: {localize(props.event.closeRegistration).format('MMMM, Do h:mm a')}
         </div>
         <div onClick={()=>{props.onClick(props.event.id)}}>
             View Registration Data
@@ -144,16 +153,18 @@ class RecentEventsInner extends Component{
         this.setState({showPopup:false, eventId:undefined})
     }
     render = () => {
-        const events = this.props.data.eventInDates.nodes.map((event)=>{
+        const events = this.props.data.eventInDates.nodes.sort((a,b)=>a.closeRegistration<b.closeRegistration).map((event)=>{
             return <AdminEvent event={event} onClick={this.showPopup} key={event.id}/>
         })
-        return <div>
+        return <div className='recent-events-container'>
         <Popup open={this.state.showPopup} closeOnDocumentClick onClose={this.clearPopupState} className='main-contents'>
             <ReactQuery query={EVENT_REGISTRATIONS(this.state.eventId)}>
-                <RecentEventsStudents/>
+                <RecentEventDays/>
             </ReactQuery>
         </Popup>
+        <GridView itemsPerRow={3}>
             {events}
+        </GridView>
         </div>
     }
 }
