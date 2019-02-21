4cf721f5d9b54a4b15512dba399b9e046f2a245a
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-02-20 18:05:03 -0600

    implement the intended function of manage users

--- src/app/main/manageUsers/ManageUsers.js
+++ src/app/main/manageUsers/ManageUsers.js
@@ -3,14 +3,18 @@ import Mutation from '../../../delv/Mutation'
 import {DropDown, BasicPopup, SecureRoute, GridView} from '../common/Common';
 import {ReactQuery} from '../../../delv/delv-react'
 import moment from 'moment';
-import OrderHistory from '../orderHistory/OrderHistory'
+import {StatelessOrderHistory} from '../orderHistory/OrderHistory'
 import './ManageUsers.css'
 import alert from '../../logos/alert.svg'
 import Popup from "reactjs-popup"
+import {StudentWaiver} from '../studentWaiver/StudentWaiver'
+import next from '../../logos/next.svg'
+import DateTime from 'react-datetime'
+const yearAgo = moment().subtract(1,'years').toISOString()
+
 const GET_USERS = `{
   allUsers{
     nodes{
-      nodeId
       id
       firstName
       lastName
@@ -22,7 +26,6 @@ const GET_USERS = `{
 const GET_USER_DATA = (userId) => `{
   allUsers(condition:{id:"${userId}"}){
     nodes{
-      nodeId
       id
       createdOn
       role
@@ -31,21 +34,7 @@ const GET_USER_DATA = (userId) => `{
       email
       refundRequestsByUserId{
         nodes{
-          nodeId
           id
-          reason
-          amountRefunded
-          grantedReason
-          status
-          createdOn
-        }
-      }
-      studentsByParent{
-        nodes{
-          nodeId
-          id
-          firstName
-          lastName
         }
       }
     }
@@ -55,8 +44,40 @@ const GET_USER_DATA = (userId) => `{
 const UPDATE_USER_ROLE = (userId, role) => `mutation($id:UUID!, $role:RoleType!){
   updateUserById(input:{id:$id, userPatch:{role:$role}}){
     user{
-      nodeId
+      id
       role
+    }
+  }
+}`
+
+const GET_STUDENTS = (parentId) => `{
+  allStudents(condition: {parent: "${parentId}"}) {
+    nodes {
+      id
+      firstName
+      parent
+      dateOfBirth
+      lastName
+      studentWaiversByStudent(filter: {createdOn: {greaterThan: "${yearAgo}"}}) {
+        nodes {
+          id
+          createdOn
+          primaryCare
+          primaryCarePhone
+          pickupOne
+          pickupTwo
+          emergencyPhone
+          other
+        }
+      }
+    }
+  }
+}`
+
+const CREATE_OVERRIDE = `mutation($override:RegistrationOverrideInput!){
+  createRegistrationOverride(input:{registrationOverride:$override}){
+    registrationOverride{
+      id
     }
   }
 }`
@@ -72,9 +93,6 @@ const DROPDOWN_OPTIONS = [
         name:'Attendant',
         value:'FTLC_ATTENDANT'
     },{
-        name:'Lead instructor',
-        value:'FTLC_LEAD_INSTRUCTOR'
-    },{
         name:'Admin',
         value:'FTLC_ADMIN'
     },{
@@ -83,18 +101,18 @@ const DROPDOWN_OPTIONS = [
     }
 ]
 
-class ManageUserForm extends Component{
+class UserForm extends Component{
     constructor(props){
         super(props)
         this.state = {
-            promote:props.allUsers.nodes[0].role,
-            student:undefined
+            promote:props.user.role
         }
         this.promoteMutation = new Mutation({
-            mutation:UPDATE_USER_ROLE(this.props.id, this.state.promote),
+            mutation:UPDATE_USER_ROLE(this.props.user.id, this.state.promote),
             onSubmit:this.promoteUser
         })
     }
+
     handleChange = (event) => {
         const target = event.target;
         const value = target.type === 'checkbox'
@@ -106,65 +124,165 @@ class ManageUserForm extends Component{
     promoteUser = (event) => {
         event.preventDefault()
         if(this.state.promote){
-            return{id:this.props.id, role:this.state.promote}
+            return{id:this.props.user.id, role:this.state.promote}
         }
         return false;
     }
-    setSelectedStudents = (student) =>{
-        this.setState({selectedStudent:student});
+    render = () => {
+        const user = this.props.user
+        return <div>
+            <h2>Account</h2>
+            <table className='manage-user-table'>
+                <tbody>
+                    <tr>
+                        <td>Name: </td>
+                        <td>{user.firstName} {user.lastName}</td>
+                        <td>Email: </td>
+                        <td>{user.email}</td>
+                    </tr>
+                    <tr className='no-wrap'>
+                        <td>Memeber since: </td>
+                        <td>{moment(user.createdOn).format('MMM, Do YYYY')}</td>
+                        <td>ID: </td>
+                        <td>{user.id}</td>
+                    </tr>
+                    <tr>
+                        <td>role: </td>
+                        <td>{user.role}</td>
+                        <td>promote to: </td>
+                        <td><DropDown options={DROPDOWN_OPTIONS} value={this.state.promote} name='promote' onChange={this.handleChange} /> <button onClick={this.promoteMutation.onSubmit}>promote</button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    }
+
+}
+
+class OverrideForm extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            event:'',
+            modifiedPrice:0,
+            validEnd: moment().add(1, 'months')
+        }
+        this.mutation = new Mutation({
+            mutation:CREATE_OVERRIDE,
+            onSubmit:this.handleSubmit,
+            onResolve:this.resetState
+        })
+    }
+    componentWillUnmount = () => {
+        this.mutation.removeListeners()
+    }
+    handleTimeChange = (key, value) => {
+        this.setState({[key]: value})
+    }
+
+    handleChange = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox'
+            ? target.checked
+            : target.value;
+        const name = target.name;
+        this.setState({[name]: value});
+    }
+    hasRequiredValues = () => {
+        return this.state.event
+    }
+    handleSubmit = (event) => {
+        event.preventDefault()
+        if(this.hasRequiredValues){
+            return {override:{...this.state, student:this.props.studentId}}
+        }
+    }
+    resetState = () => {
+        this.setState({
+            event:'',
+            modifiedPrice:0,
+            validEnd: moment().add(1, 'months')
+        })
+    }
+    render = () => {
+        return <form className='override-form' onSubmit={this.mutation.onSubmit}>
+            <h2>Override</h2>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Event Id</td>
+                        <td><input value={this.state.event} onChange={this.handleChange} name='event'placeholder='Event Id'/></td>
+                    </tr>
+                    <tr>
+                        <td>New Price: (optional)</td>
+                        <td><input value={this.state.modifiedPrice} onChange={this.handleChange} name='modifiedPrice' type='number'/></td>
+                    </tr>
+                    <tr>
+                        <td>Valid until:</td>
+                        <td><DateTime className="full-date-input" dateFormat="MMM Do YYYY" timeFormat="H:mm"  value={this.state.validEnd} onChange={(time) => {this.handleTimeChange("validEnd", time)}}/></td>
+                    </tr>
+                </tbody>
+            </table>
+            <div className='styled-button' onClick={this.mutation.onSubmit}>Create</div>
+        </form>
+    }
+}
+
+function StudentForm(props){
+    const studentWaiver = props.studentWaiversByStudent.nodes[0]
+    return<div>
+        <h3 className='no-margin'>{props.firstName} {props.lastName}</h3>
+        <div className='section container'>
+            <OverrideForm studentId={props.id} />
+            <div className='section'>
+                <StudentWaiver {...studentWaiver}/>
+            </div>
+            <div className='section'>
+                <h2>Student info</h2>
+                <div>{props.firstName} {props.lastName}</div>
+                Date of Birth: {moment(props.dateOfBirth).format('MMMM, YYYY')}
+            </div>
+        </div>
+    </div>
+}
+
+function StudentsForm(props){
+    return props.allStudents.nodes.map((student)=><StudentForm key={student.id} {...student}/>)
+}
+
+class ManageUserForm extends Component{
+    constructor(props){
+        super(props)
+        this.state = {UI:'default'}
     }
     render = () => {
         const user = this.props.allUsers.nodes[0]
-        return <div className='manage-user-form'>
-        <h1>{this.props.firstName} {this.props.lastName}</h1>
-            <div>
-                <h2>Account</h2>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>Name: </td>
-                            <td>{user.firstName} {user.lastName}</td>
-                        </tr>
-                        <tr>
-                            <td>ID: </td>
-                            <td>{user.id}</td>
-                        </tr>
-                        <tr>
-                            <td>Email: </td>
-                            <td>{user.email}</td>
-                        </tr>
-                        <tr className='no-wrap'>
-                            <td>Memeber since: </td>
-                            <td>{moment(user.createdOn).format('MMM, Do YYYY')}</td>
-                        </tr>
-                        <tr>
-                            <td>role: </td>
-                            <td>{user.role}</td>
-                        </tr>
-                        <tr>
-                            <td>promote to: </td>
-                            <td><DropDown options={DROPDOWN_OPTIONS} value={this.state.promote} name='promote' onChange={this.handleChange} /></td>
-                            <td><button onClick={this.promoteMutation.onSubmit}>promote</button></td>
-                        </tr>
+        if(this.state.UI === 'students'){
+            return<div className='manage-user-popup'>
+                <h1>{user.firstName} {user.lastName}s Students</h1> // TODO add '
+                <ReactQuery query={GET_STUDENTS(user.id)}>
+                    <StudentsForm/>
+                </ReactQuery>
+                <div className='back-container' onClick={()=>{this.setState({UI:'default'})}}> <img className='previous back-arrow' src={next} title='Icon made by Gregor Cresnar'/> Back</div>
+            </div>
+        }else if(this.state.UI === 'orders'){
+            return <div className='manage-user-popup'>
+                <h1>{user.firstName} {user.lastName}s order history</h1>
+                <StatelessOrderHistory userId={user.id} adminForm/>
+                <div className='back-container' onClick={()=>{this.setState({UI:'default'})}}> <img className='previous back-arrow' src={next} title='Icon made by Gregor Cresnar'/> Back</div>
+            </div>
+        }
+        return <div className='manage-user-popup'>
+            <h1>{user.firstName} {user.lastName}</h1>
+            <UserForm user={user} />
+            <div className='manage-user-btn-container'>
+                <span className='styled-button' onClick={()=>{this.setState({UI:'orders'})}}>View orders</span>
+                <span className='styled-button' onClick={()=>{this.setState({UI:'students'})}}>View Students</span>
+            </div>
+    </div>
 
-                    </tbody>
-                </table>
-            </div>
-            <div>
-                <h2>Order History</h2>
-                <OrderHistory userId={user.id} adminForm={true}/>
-            </div>
-            {user.studentsByParent.nodes.length > 0?<div>
-                <h2>Students</h2>
-            </div>:<div>
-            <h2>This user has no students</h2>
-        </div>}
-        </div>
     }
 }
-// <DropDown options={this.props.students} value={this.state.student} name='student' onChange={this.handleChange} />
-
-
 
 const itemsPerRow = 5;
 class ManageUsersInner extends Component {
