f12e7d096a6c937baace763aa9737847fa97bc74
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-02-23 00:34:09 -0600

    finish logout

--- src/app/main/logout/Logout.js
+++ src/app/main/logout/Logout.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react'
 import axios from 'axios'
-import Delv from '../../../delv/delv';
+import Delv from '../../../delv/delv'
 class Logout extends Component {
-  componentDidMount () {
-    axios.post('http://localhost:3005/logout').then((res)=>{
-        Delv.clearCache()
-        window.location.href = '/'
-    })
-  }
-  render () {
-    return (<div>Logging Out</div>)
-  }
+    componentDidMount () {
+        axios.post('http://localhost:3005/logout').then(()=>{
+            Delv.clearCache()
+            window.location.href = '/'
+        })
+    }
+    render () {
+        return <div>Logging Out</div>
+    }
 }
 
 export default Logout
