26bba88b232545aa3b3e6c1db76d8dc7cf32d516
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-02-23 00:55:26 -0600

    finish activity

--- src/app/main/activities/activity/Activity.js
+++ src/app/main/activities/activity/Activity.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import './Activity.css'
-import { Link } from 'react-router-dom'
 function Activity (props) {
-return <div className='activity-card' onClick={()=>{props.history.push(`/Events/${props.name}/${props.id}`)}}>
-            <img className='activity-image mobile-center-x' src={props.url || 'https://via.placeholder.com/350x150'} />
-      <h2>{props.name}</h2>
-      <div>
-          <h4 className='no-margin'>Prerequisites</h4>
-          {props.prerequisites}
-      </div>
-      <div className='activity-body'>{props.description}</div>
-  </div>
+    return <div className='activity-card' onClick={()=>{props.history.push(`/Events/${props.name}/${props.id}`)}}>
+        <img className='activity-image' src={props.url || 'https://via.placeholder.com/350x150'} />
+        <h2>{props.name}</h2>
+        <div>
+            <h4 className='no-margin'>Prerequisites</h4>
+            {props.prerequisites}
+        </div>
+        <div className='activity-body'>{props.description}</div>
+    </div>
 }
 
 export default Activity
