c1e4e8ff0939a3351bad6c9b91af875931b9eb69
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-03-05 23:26:30 -0600

    finish event logs

--- src/app/main/eventLogs/EventLogs.js
+++ src/app/main/eventLogs/EventLogs.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import moment from 'moment'
 import { Location, GridView, SecureRoute} from '../common/Common'
 import './EventLogs.css'
-import gql from 'graphql-tag'
 import {ReactQuery} from '../../../delv/delv-react'
 import InstructorLogForm from '../instructorLogs/InstructorLogs'
 // THIS IS NOT TESTED YET
@@ -49,8 +48,7 @@ const localize = (timestamp) =>{
 }
 
 function EventLog(props){
-
-    return <div className="event-log-container">
+    return <div className="card event-log-card">
         <h3>{localize(props.date.start).format('MMMM, Do h:mm a')} - {localize(props.date.end).format('h:mm a')}</h3>
         {props.logs.map(log =>{
             const instructor = (log.instructor && log.instructor.firstName) || 'Attendant'
@@ -83,11 +81,11 @@ function EventLogsInner(props) {
             }
         }
     })
-    return <div className='main-contents'>
-        <h2>{activityName} {student.firstName} {student.lastName}</h2>
-        <h3>{event.name}</h3>
-        <GridView fillerStyle={"event-log-container filler"} itemsPerRow={3}>
-                {Object.values(dates).sort((a,b)=>a.date.start<b.date.start).map(date=><EventLog key={date.date.id} user={props.getUserData} {...date} />)}
+    return <div className='main-contents column container'>
+        <h2>{student.firstName} {student.lastName}</h2>
+        <h3>{activityName}  {event.name}</h3>
+        <GridView fillerStyle='card event-log-card' itemsPerRow={3}>
+                {Object.values(dates).sort((a,b)=>moment(b.date.start).unix() - moment(a.date.start).unix()).map(date=><EventLog key={date.date.id} user={props.getUserData} {...date} />)}
         </GridView>
     </div>
 }
