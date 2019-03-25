1c530e299965e9969507cbab03e4bb4cdc888711
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-03-25 17:15:50 -0500

    copywrite and move padding to border

--- src/app/main/home/whoWeAre/WhoWeAre.js
+++ src/app/main/home/whoWeAre/WhoWeAre.js
@@ -8,11 +8,15 @@ function WhoWeAre(props){
             <div className='who-we-are-container'>
                 <div className='who-we-are-section'>
                     <h2>Our Goal</h2>
-                    Our goal at Fun 2 Learn code is to provide educational, and fun
+                    Fun 2 Learn Code offers a deiverse selection of project-based learning experiences focused on students intrests.
+                    Our courses are created in house to ensure students are learning the most relevant and up to date topics in each class.
+                    We strive to extends accessablity if code concepts to all those who are intrested, and work hard to ensure every student
+                    has the opprotunity to learn, not matter the topic.
                 </div>
                 <div className='who-we-are-section'>
                     <h2>About us</h2>
-                    Founded in 2013
+                    Founded in 2013 by Manny Castro, Fun 2 Learn Code's roots stem from the need for accessable and afforable summer camp options
+                    in the programming field. 
                 </div>
             </div>
         </div>
