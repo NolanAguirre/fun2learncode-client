94d2ee00fe1f7ea97881f84ba8452591dc4d17cd
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-04-12 21:49:01 -0500

    refactor to use graphql

--- src/app/main/refund/RefundResponse.js
+++ src/app/main/refund/RefundResponse.js
@@ -5,20 +5,39 @@ import axios from 'axios'
 import './Refund.css'
 import Popup from "reactjs-popup"
 import xicon from '../../logos/x-icon.svg'
+
+const UPDATE_CACHE = (id) => `{
+  allRefundRequests (condition:{id:"${id}"}){
+    nodes {
+      id
+      reason
+      amountRefunded
+      grantedReason
+      status
+      createdOn
+      paymentByPayment{
+        id
+      }
+    }
+  }
+}`
+
+const REFUND_RESPONSE = `mutation($grantedReason:String!, $paymentId:UUID!, $amount:Float!, $unregister:Boolean!, $grant:Boolean!){
+  refundTransaction(grantedReason:$grantedReason, paymentId:$paymentId, amount:$amount, unregister:$unregister, grant:$grant)
+}`
+
+
 class RefundResponse extends Component{
 	constructor(props){
 		super(props);
-		this.state = {grantedReason:'', grant:false, amountRefunded:this.props.total, remove:true}
+		this.state = {grantedReason:'', grant:false, amount:this.props.total, unregister:true}
+        this.mutation = new Mutation({
+            mutation:REFUND_RESPONSE,
+            onSubmit:this.handleSubmit,
+            onResolve:this.handleResolve
+        })
 	}
-
-    componentDidMount = () => {
-        this.isMounted = true
-    }
-
-    componentWillUnmount = () => {
-        this.isMounted = false
-    }
-
+    componentWillUnmount = () => this.mutation.removeListeners()
 	handleChange = (event) => {
         const target = event.target;
         const value = target.type === 'checkbox'
@@ -42,46 +61,48 @@ class RefundResponse extends Component{
 			return
 		}
 		if(window.confirm('Are you sure? this account cannot be undone or edited after submission.')){
-			axios.post('http://localhost:3005/payment/refund', {
-				grantReason:this.state.grantedReason,
-				paymentId:this.props.paymentId,
-				amount: (this.state.grant)?this.state.amountRefunded:0,
-				unregister: this.state.remove,
-                grant:this.state.grant
-			})
+            return {paymentId:this.props.paymentId,...this.state}
 		}
+        return false
 	}
 
+    handleResolve = (data, error) => {
+        console.log(data)
+        if(error){
+
+        }else{
+            return UPDATE_CACHE(data.refundTransaction)
+        }
+    }
+
 	render = () => {
+        console.log(this.props)
         if(this.state.complete){
-            return <div className='refund-complete'>
+            return <div className='popup-inner-complete'>
                 Refund request has been send.
             </div>
         }else{
-            return <div className='payment-container'>
-                    <div>Refund Status: {this.props.status}</div>
-                    <div>Reason for refund:</div>
-                    <div>{this.props.reason}</div>
-                    <div className='error center-text'>{this.state.error}</div>
-                    Reason for grant:
-                    <textarea className='refund-textarea' value={this.state.grantReason} name='grantReason' onChange={this.handleChange}></textarea>
-                    <form onSubmit={this.handleSubmit}>
+            return <form className='payment-container' onSubmit={this.mutation.onSubmit}>
+                        <div>Refund Status: {this.props.status}</div>
+                        <div>Reason for refund:</div>
+                        <div>{this.props.reason}</div>
+                        <div className='error center-text'>{this.state.error}</div>
+                        Reason for grant:
+                        <textarea className='refund-textarea' value={this.state.grantedReason} name='grantedReason' onChange={this.handleChange}></textarea>
                         <div style={{height:'40px'}}>
 
                             Grant:<input checked={this.state.grant} name='grant' type='checkbox'  onChange={this.handleChange}/>
                             {this.state.grant?
-                                <React.Fragment>Amount:<input style={{width:'80px'}} type='number' name='amountRefunded' value={this.state.amountRefunded} onChange={this.handleChange}/> of {this.props.total}
+                                <React.Fragment>Amount:<input style={{width:'80px'}} type='number' name='amount' value={this.state.amount} onChange={this.handleChange}/> of {this.props.total}
                             </React.Fragment>:''}
                             {this.state.grant?<div>
-                                Remove registration:<input checked={this.state.remove} name='remove' type='checkbox'  onChange={this.handleChange}/>
+                                Unregister:<input checked={this.state.unregister} name='unregister' type='checkbox'  onChange={this.handleChange}/>
                             </div>:''}
                         </div>
-                        <div className='styled-button center-text margin-top-10' onClick={this.handleSubmit}>Submit response</div>
+                        <div className='styled-button center-text margin-top-10' onClick={this.mutation.onSubmit}>Submit response</div>
                         <button className='hacky-submit-button' type='submit'/>
                     </form>
-                </div>
         }
-
 	}
 }
 
